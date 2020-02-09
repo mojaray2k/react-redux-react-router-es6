9b6f78fc5bf7aa51aefa7e0622abec68deabbb93
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-09 17:12:59 -0500

    feat: implimented a mock api and async Redux middleware
    Setup Mock API - Express and json-server,  Added Redux middleware for async call Redux-Thunk , and
    created first thunks

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 export default function configureStore(initialState) {
@@ -9,6 +10,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
