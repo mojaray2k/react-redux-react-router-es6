724129e2544b3cd624a964be4d65b1b6be1e2f17
Amen Ra <amenra@USRMonitors-MacBook-Pro.local>
2020-02-16 18:06:26 -0500

    perf: fixed apps issues
    Added loading indicator, display feedback upon clicking save, handling and displaying api failures,
    handled and displayed obth server side and client side validation, and implimented optimistic
    deletes.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import PropTypes from "prop-types";
+import { Redirect } from "react-router-dom";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
-import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
-import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 class CoursesPage extends React.Component {
   state = {
     redirectToAddCoursePage: false
@@ -26,20 +28,39 @@ class CoursesPage extends React.Component {
       });
     }
   }
+
+  handleDeleteCourse = async course => {
+    toast.success("Course deleted");
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
+      toast.error("Delete failed." + error.message, { autoClose: false });
+    }
+  };
+
   render() {
-    const { courses } = this.props;
+    const { courses, loading } = this.props;
     return (
       <React.Fragment>
         {this.state.redirectToAddCoursePage && <Redirect to="course" />}
         <h2>Courses</h2>
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={courses} />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <React.Fragment>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList
+              onDeleteClick={this.handleDeleteCourse}
+              courses={courses}
+            />
+          </React.Fragment>
+        )}
       </React.Fragment>
     );
   }
@@ -48,7 +69,8 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 function MapStateToProps(state) {
@@ -62,7 +84,8 @@ function MapStateToProps(state) {
               authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors
+    authors: state.authors,
+    loading: state.apiCallsInProgress > 0
   };
 }
 
@@ -70,7 +93,8 @@ function MapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
     }
   };
 }
